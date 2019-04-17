f36cfd3a3e2ca020e07e304b633d4e1045b9ec6f
angpao <angpao.kittimasak@gmail.com>
2019-04-17 13:38:28 +0700

    Add function encrypt password Add Validate email Refor User Model

--- models/user.model.js
+++ models/user.model.js
@@ -1,11 +1,40 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
 const Schema = mongoose.Schema
 
 let UserSchema = new Schema({
-    fullName: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    fullName: {
+        type: String,
+        required: 'Full Name can\'t be empty'
+    },
+    email: {
+        type: String,
+        required: 'Email can\'t be empty',
+        unique: true
+    },
+    password: {
+        type: String,
+        required: 'Password can\'t be empty',
+        minlength: [4, 'Password must be atleast 4 character long']
+    },
     saltSecret: String
+})
+
+// Custom validation for email
+UserSchema.path('email').validate((val) => {
+    emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return emailRegex.test(val);
+}, 'Invalid e-mail.');
+
+// Encrypt for password
+UserSchema.pre('save', function (next) {
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(this.password, salt, (err, hash) => {
+            this.password = hash
+            this.saltSecret = salt
+            next()
+        })
+    })
 })
 
 module.exports = mongoose.model('User', UserSchema, 'users')
