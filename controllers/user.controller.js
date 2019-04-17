c6039d8cf4fd573311a63d5aa5d7b5fae431974a
angpao <angpao.kittimasak@gmail.com>
2019-04-17 14:16:31 +0700

    Create function authenticate

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -39,3 +39,15 @@ module.exports.getAllUsers = (req, res, next) => {
         })
     })
 }
+
+module.exports.authenticate = (req, res, next) => {
+    // call for passport authentication
+    passport.authenticate('local', (err, user, info) => {
+        // error from passport middleware
+        if (err) return res.status(400).json(err);
+        // registered user
+        else if (user) return res.status(200).json({ "token": user.generateJwt() });
+        // unknown user or wrong password
+        else return res.status(404).json(info);
+    })(req, res);
+}
