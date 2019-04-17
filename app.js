c961f4c993fe3761c7e6c83f78601a6432d0376d
angpao <angpao.kittimasak@gmail.com>
2019-04-17 13:06:07 +0700

    move connect database to controller

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-require('./configs/config')
+
 
 const express = require('express')
 const cors = require('cors')
-const mongoose = require('mongoose')
+
 
 const rtsUser = require('./routes/user.route')
 
@@ -11,10 +11,7 @@ app.use(express.json())
 app.use(cors())
 app.use('/user', rtsUser)
 
-mongoose.connect(process.env.MONGODB_URI, (err) => {
-    if (!err) { console.log('MongoDB connection succeeded. ') }
-    else { console.log('Error in MongoDB connection : ' + JSON.stringify(err, undefined, 2)) }
-})
+
 
 app.listen(process.env.PORT, () => {
     console.log(`Server started at port : ${process.env.PORT}`)
--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,11 +1,19 @@
+require('../configs/config')
+
 const mongoose = require('mongoose')
 const User = require('../models/user.model')
 
 module.exports.register = (req, res, next) => {
-    const user = new User(req.body)
-    user.save((err, doc) => {
-        if (!err) {
-            res.status(201).send(doc)
-        }
+    mongoose.connect(process.env.MONGODB_URI, (err) => {
+        if (!err) { console.log('MongoDB connection succeeded. ') }
+        else { console.log('Error in MongoDB connection : ' + JSON.stringify(err, undefined, 2)) }
+    }).then(() => {
+        const user = new User(req.body)
+        user.save((err, doc) => {
+            if (!err) {
+                res.status(201).send(doc)
+            }
+        })
     })
+
 }
